b7031f8b812a702918a5d7fd4c4e5c69bac4d6b5
arweeta <94476382+arweeta@users.noreply.github.com>
2022-10-07 11:55:41 +0100

    created base for index & pseudocode in js & css

---
+++ script.js
@@ -0,0 +1,24 @@
+//DOM elements (querySelectors)
+
+//home page with start game button
+
+//noughts and crosses and toggle between taking turns
+
+//function to take a turn
+//if box empty & selected write noughts or crosses
+//once turn taken, toggle to other player
+//check for winner
+
+//function to reset the board
+//clear all boxes
+//display winner function
+
+//function to check for winner/loser or draw
+//using arrays to check for winner
+//if no winner == draw
+//end game and show display winner screen
+
+//function to display winner
+//have an alert type message
+//replay button
+//home button
