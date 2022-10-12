1360ada668bb713398d4f3e82855eafdb49cce61
arweeta <94476382+arweeta@users.noreply.github.com>
2022-10-12 05:11:09 +0100

    function to check for winner - incomplete

--- script.js
+++ script.js
@@ -28,9 +28,22 @@ boxes.forEach( (e) => { e.addEventListener('click',
 });
 
 
-//if box empty & selected write noughts or crosses
-//once turn taken, toggle to other player
 //check for winner
+const winningCombos =[
+    //Horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    //Verticle
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    //Diaganol
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+
 
 //function to reset the board
 //clear all boxes
@@ -45,10 +58,33 @@ const resetBoard = () => {
 // Event Listener
 reset.addEventListener('click', resetBoard)
 
+
 //function to check for winner/loser or draw
+const checkForWinner = () => {
+    let xArr = [];
+    let oArr = [];
+    // create arrays of box numbers for Xs and Os
+    boxes.forEach(
+        (box) => {
+            if ( box.textContent ) {
+                if ( box.textContent == 'X' ) {
+                    xArr.push(parseInt(box.id));
+                }    
+                if ( box.textContent == 'O' ) {
+                    oArr.push(parseInt(box.id));   
+                }    
+            }         
+        }
+    );
+
+
 //using arrays to check for winner
+const declareWinner = (win) => {
+    alertBox.innerHTML = 
+}
 //if no winner == draw
 //end game and show display winner screen
+
 
 //function to display winner
 //have an alert type message
